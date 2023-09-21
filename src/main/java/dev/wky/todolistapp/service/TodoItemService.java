86a84365a046bd9f77df196ac36d6f805bde68bb
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2023-09-21 15:27:51 -0300

    Atualizando TodoItem.java Criado e implementado TodoItemService.java

--- src/main/java/dev/wky/todolistapp/service/TodoItemService.java
+++ src/main/java/dev/wky/todolistapp/service/TodoItemService.java
@@ -1,4 +1,41 @@
 package dev.wky.todolistapp.service;
 
-public record TodoItemService() {
+import dev.wky.todolistapp.model.TodoItem;
+import dev.wky.todolistapp.repository.TodoItemRepository;
+import dev.wky.todolistapp.repository.TodoListRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public record TodoItemService(
+        TodoItemRepository todoItemRepository,
+        TodoListRepository todoListRepository
+) {
+    public TodoItem createTodoItem(TodoItem todoItem) {
+        return todoItemRepository.save(todoItem);
+    }
+
+    public TodoItem getTodoItem(UUID todoItemId) {
+        return todoItemRepository.findById(todoItemId).orElseThrow(
+                () -> new IllegalStateException("TodoItem with id " + todoItemId + "does not exist")
+        );
+    }
+
+    public void deleteTodoItem(UUID todoItemId) {
+        todoItemRepository.deleteById(todoItemId);
+    }
+
+    public TodoItem updateTodoItem(TodoItem todoItem) {
+        TodoItem existingTodoItem = getTodoItem(todoItem.getId());
+        existingTodoItem.setDescription(todoItem.getDescription());
+        existingTodoItem.setCompleted(todoItem.isCompleted());
+        return todoItemRepository.save(existingTodoItem);
+    }
+    public TodoItem markTodoItemAsCompleted(UUID todoItemId) {
+        TodoItem existingTodoItem = getTodoItem(todoItemId);
+        existingTodoItem.setCompleted(true);
+        return todoItemRepository.save(existingTodoItem);
+    }
 }
